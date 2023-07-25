80cee62b37d8fdf5145065beac586934fd9cd498
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2023-07-25 03:14:14 -0400

    New script

---
+++ script/proyecto.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+ funcion=$1
+viewer=$2
+
+case $funcion in
+
+run)
+cd ..
+make dev
+cd script
+;;
+report)
+cd ..
+cd informe
+pdflatex informe.tex
+cd ..
+cd script
+;;
+slides)
+cd ..
+cd presentacion
+pdflatex Presentacion.tex
+cd ..
+cd script
+;;
+show_report)
+     
+    case "$viewer" in
+
+        evince) evince informe.pdf 
+;;
+        okular) okular informe.pdf 
+;;
+        xpdf) xpdf informe.pdf 
+;;
+        *) okular informe.pdf
+;;
+
+esac
+
+;;
+show_slides)
+
+    case "$viewer" in
+
+        evince) evince Presentacion.pdf 
+;;
+        okular) okular Presentacion.pdf 
+;;
+        xpdf) xpdf Presentacion.pdf 
+;;
+        *) okular Presentacion.pdf
+;;
+
+esac
+
+;;
+
+clean)
+cd ..
+rm */*
+git reset --hard
+cd script
+;;
+
+*) echo "Elija una opcion válida"
+;;
+
+esac
